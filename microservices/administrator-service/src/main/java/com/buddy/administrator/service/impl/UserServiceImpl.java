0cf79c03970d3893e7651c9f072037eb0e0f72c9
unknown <akarmakar891@gmail.com>
2026-02-09 23:23:02 +0530

    ..

--- microservices/administrator-service/src/main/java/com/buddy/administrator/service/impl/UserServiceImpl.java
+++ microservices/administrator-service/src/main/java/com/buddy/administrator/service/impl/UserServiceImpl.java
@@ -1,15 +1,25 @@
 package com.buddy.administrator.service.impl;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.administrator.client.dto.request.UserCreateRequest;
 import com.buddy.administrator.repository.UserRepository;
+import com.buddy.administrator.service.UserService;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-public class UserServiceImpl {
+public class UserServiceImpl implements UserService {
 	private final UserRepository userRepository;
+	private final ModelMapper modelMapper;
+
+	@Override
+	public ResponseEntity<?> createUser(UserCreateRequest userCreateDto) {
+		
+	}
 	
 	
 }
